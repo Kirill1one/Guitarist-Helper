df7a44ae16859946b955396d8093d5217614798c
Kirill1one <kirill2002ku@gmail.com>
2024-09-19 20:13:07 +0300

    Added direction of arrows

--- js/script.js
+++ js/script.js
@@ -7,6 +7,11 @@ function updateNumber() {
 	const randomNumber = anyNumber() // Получаем случайное число
 	const rootElement = document.getElementById('root')
 	rootElement.innerHTML = randomNumber
+	const directionArrow = randomNumber > 5 ? 'up' : 'down' // Направление стрелки
+
+	const symbolArrow = directionArrow === 'up' ? '↑' : '↓'
+
+	rootElement.innerHTML += `${symbolArrow}` // Добавляем стрелку
 }
 
 setInterval(updateNumber, 1000)
